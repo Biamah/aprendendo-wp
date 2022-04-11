87890fa8838d793b83b847125ac3976468b318ed
Biamah <beatriz.souza.costa@outlook.com>
2022-04-11 19:17:39 -0300

    feat: ajusta estilo do conteudo de page about

--- wp-content/themes/aprendendoWp/index.php
+++ wp-content/themes/aprendendoWp/index.php
@@ -4,12 +4,12 @@ require_once 'header.php';
 
 if(have_posts()):
     ?>
-    <main class="main-sobre-nos">
+    <main class="b-about">
         <?php
         while(have_posts()): the_post();
-            the_post_thumbnail('post-thumbnail', array('class' => 'imagem-sobre-nos')); 
-            echo '<div class="conteudo container">';
-            the_title('<h2>', '</h2>');
+            the_post_thumbnail('post-thumbnail', array('class' => 'c-about__image')); 
+            echo '<div class="c-about__conteudo container">';
+            the_title('<h2 class="c-about__title">', '</h2>');
             the_content();
             echo '</div>';
         endwhile;
